e4e1f2f5b60b63d8bb70cb502c766d9bba7cee5e
Marc V. <marckarella13@gmail.com>
2026-03-16 12:52:57 -0700

    find_batch_files complete

--- data_clean.py
+++ data_clean.py
@@ -1,14 +1,25 @@
 import os
 
 
-def find_batch_files(csv):
+def find_batch_files(category, scenarios_dir):
     """
     Scan the scenario directory and return a list of full file paths
     for all batch CSVs belonging to the given category.
 
-    Excludes the master CSV that does not contain a 'batch' name"""
+    Excludes the master CSV that does not contain a 'batch' name
+    """
+    batch_files = []
 
-    pass
+    for filename in os.listdir(scenarios_dir):
+        if (
+            filename.startswith(category)
+            and filename.endswith(".csv")
+            and "batch" in filename
+        ):
+            full_path = os.path.join(scenarios_dir, filename)
+            batch_files.append(full_path)
+
+    return batch_files
 
 
 def load_and_merge(csv):
